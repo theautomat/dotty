35ef61f62df9ada4a981df249e7f6c907adfedd6
theautomat <kurt@automatique.la>
2025-11-11 12:55:34 -0800

    Update test token mint from dev:local script

--- src/config/solana.ts
+++ src/config/solana.ts
@@ -8,5 +8,5 @@ export const SOLANA_CONFIG = {
   PROGRAM_ID: '7fcqEt6ieMEgPNQUbVyxGCpVXFPfRsj7xxHgdwqNB1kh',
 
   // Test token mint - updated by dev:local script
-  TEST_TOKEN_MINT: 'AwY6h4Cw2L7wrNyEGeUmRq4pgzUBxvXQEfpDtxubskUF',
+  TEST_TOKEN_MINT: 'C2RLfGiBhcqffifs4XmARyAuqnLFuHECfGw6E7Kp5EEL',
 };
