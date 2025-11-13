f03c55f1d89050fa9b44b03d05f3a66e47abaafe
theautomat <kurt@automatique.la>
2025-11-12 21:52:55 -0800

    add stuff

--- src/config/solana.ts
+++ src/config/solana.ts
@@ -8,8 +8,8 @@ export const SOLANA_CONFIG = {
   PROGRAM_ID: '7fcqEt6ieMEgPNQUbVyxGCpVXFPfRsj7xxHgdwqNB1kh',
 
   // TREASURE token mint - the meme token that players hide/find - updated by dev:local script
-  TREASURE_TOKEN_MINT: 'AY9dw9XbTwWLYtYaaT24GmdRSXhGDDXyHRFYCdY6F2Ae',
+  TREASURE_TOKEN_MINT: 'DdTEnRwoj4hUDouDkYYtC5eR4tEmXy3LaJoaV3RXamrF',
 
   // BOOTY token mint - in-game currency for search fees - updated by dev:local script
-  BOOTY_TOKEN_MINT: '46PfJtzidWBdxmQAR4kFbarjXPZkhUrA1GP82t3eHBFs',
+  BOOTY_TOKEN_MINT: '5W46xmMCUXXvppoidcTaGZaff38T1yHEH1N6DkSu45tK',
 };
